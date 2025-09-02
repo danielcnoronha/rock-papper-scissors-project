c55cb9d257ca775a2ef6f4f2a2d32c9cc3196cc9
danielcnoronha <205429835+danielcnoronha@users.noreply.github.com>
2025-09-01 23:42:51 -0300

    add the second function, user choses one option and the code returns it

--- script.js
+++ script.js
@@ -14,8 +14,35 @@ function getComputerChoice () {
    
 }
 
+/*
+
+Create a prompt message that displays "rock, papper, scissors ?"
+assign values so that prompt can read "rock", "papper" or "scissors"
+create an alert that returns "you chose (Rock, papper os scissors)"
+
+*/
+
 console.log(getComputerChoice())
 
+function getHumanChoice (){
+
+    let humanChoice = prompt("Rock,papper,scissors?");
+
+    if (humanChoice === "rock"){
+        return("You chose rock!")
+    }
+    else if (humanChoice === "papper"){
+        return("You chose papper!")
+    }
+    else if (humanChoice === "scissors"){
+        return("You chose scissors!")
+    }
+    else {
+        return("You made a typo")
+    };
+}
+
+console.log(getHumanChoice())
 
 
 
